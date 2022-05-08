dbe839fcf45f6d41fca08fe1e3ff8512ba512a38
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-05-08 16:34:00 +0800

    items delevary from server side and client side

--- index.js
+++ index.js
@@ -10,21 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 function jwtVerify  (req, res, next){
-const acessToken = req.headers.authorization;
-if(!acessToken){
- return res.status(401).send({ message:'unauthorized access'})
-}
-const [bearer, token] = acessToken.split(' ');
-jwt.verify(token, process.env.ACCESS_TOKEN,(err,decoded) => {
-    if(err){
-        res.status(403).send({ message:'Forbidden access'})
-    }else{
-        req.decoded = decoded;
-        
-        next()
+    const acessToken = req.headers.authorization;
+    if(!acessToken){
+        return res.status(401).send({ message:'unauthorized access'})
     }
-})
-
+    const [bearer, token] = acessToken.split(' ');
+    jwt.verify(token, process.env.ACCESS_TOKEN,(err,decoded) => {
+        if(err){
+            res.status(403).send({ message:'Forbidden access'})
+        }else{
+            req.decoded = decoded;
+            
+            next()
+        }
+    })
 }
 
 
@@ -35,39 +34,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 const run = async () => {
-
     try {
         await client.connect();
         const inventoryCollection = client.db("warehouse").collection("inventory");
-      //  for jwt token access securly 
-       app.post('/login',async (req, res) => {
+        //  for jwt token access securly 
+        app.post('/login',async (req, res) => {
         const email = req.body;
-        // console.log(email)
         var token = jwt.sign(email, process.env.ACCESS_TOKEN);
         res.send({success: true, token})
-       })
+        })
 
-      // get all my items
+        // get all my items
         app.get('/my-items',jwtVerify, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded
-            // console.log(decodedEmail)
-            // console.log(email)
             const query = {email:email};
            
             
-           if(email === decodedEmail.email){
+            if(email === decodedEmail.email){
             const cursor = inventoryCollection.find(query)
             const myItems = await cursor.toArray()
             res.send(myItems)
-           }else{
-               res.status(403).send({message: 'Forbidden access'})
-           }
-           
-            
-        
-           
-            
+            }else{
+                res.status(403).send({message: 'Forbidden access'})
+            }
         })
         //add inventory by current user
         app.post('/add-inventory', async (req, res) => {
@@ -75,7 +65,6 @@ const run = async () => {
             const user = req.body;
             const result = await inventoryCollection.insertOne(user)
             res.send({ success: true, error: 'your inventory unSuccessfull', result })
-
         })
         //get all invetory current user
         app.get('/add-inventory', async (req, res) => {
@@ -85,18 +74,14 @@ const run = async () => {
             const inventorys = await cursor.toArray()
             res.send(inventorys)
         })
-        
         //specific user inventory details by id
         app.get('/add-inventory/:id', async (req, res) => {
-
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
 
             const result = await inventoryCollection.findOne(query);
             res.send(result)
-
         })
-
         //delete item clinet side
         app.delete('/manage-inventory/:id', async (req, res) => {
 
@@ -114,9 +99,6 @@ const run = async () => {
 
             const matched = result.filter(inventory => inventory.title.toLowerCase().includes(query));
             res.send(matched)
-
-            
-
         })
         //update item quantity add and delete
         app.put('/add-inventory/:id', async (req, res) => {
@@ -132,44 +114,36 @@ const run = async () => {
             const deleteQuantity = parseInt(result.quantity) - deleted;
 
             try {
-
                 if (add && !deleted) {
 
                     if (result.quantity) {
                         updateQuantity = await inventoryCollection.updateOne(query, { $set: { quantity: addQuantity } }, options)
-                    } else {
+                    }
+                    else {
                         updateQuantity = await inventoryCollection.updateOne(query, { $set: { quantity: add } }, options)
                     }
-
                     res.send(result)
-
-                } else {
-
+                }
+                else {
                     if (result.quantity) {
                         updateQuantity = await inventoryCollection.updateOne(query, { $set: { quantity: deleteQuantity } }, options)
-
                     }
-
                     else {
                         res.send({ error: 'Error updating quantity' })
                     }
                     res.send(result)
                 }
- }
+            }
             catch (error) {
                 console.log(error)
 
             }
 
-})
-      
+        })
     }
     finally {
 
     }
-
-
-
 }
 run().catch(console.dir)
 
